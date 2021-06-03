ddc3a3c0ff8aa1ade50b5043f742515fe84e5cdf
kokikudo <massanmesu@icloud.com>
2021-06-03 13:10:34 +0900

    コードのみでPageControllerを実装。現在のビューを示すポインターを追加

---
+++ practicePageViewController_codeOnly/SecondViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SecondViewController.swift
+//  practicePageViewController_codeOnly
+//
+//  Created by kudo koki on 2021/06/03.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let labal = UILabel()
+        let screenWidth: CGFloat = view.frame.size.width
+        let screenHeight: CGFloat = view.frame.size.height
+        labal.frame.size = CGSize(width: 100, height: 100)
+        labal.center = CGPoint(x: screenWidth/2, y: screenHeight/2)
+        
+        labal.text = "2"
+        labal.textAlignment = NSTextAlignment.center
+        self.view.addSubview(labal)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
