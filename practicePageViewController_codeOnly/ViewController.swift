f41170206fd9ccb3e0249e24998126b3fddbe7bb
kokikudo <massanmesu@icloud.com>
2021-06-03 12:18:11 +0900

    Initial Commit

---
+++ practicePageViewController_codeOnly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practicePageViewController_codeOnly
+//
+//  Created by kudo koki on 2021/06/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
